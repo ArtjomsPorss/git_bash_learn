0e7d8d62f9e197cfeeda6e9d720ada70509b7879
Art Porsh <artjoms.porss@gmail.com>
2015-12-05 12:57:06 +0000

    initial commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test{
+  public static void main(String[] args) {
+    //initial commit
+    //new comment
+    //comment before reset
+    /* comment on the branch other */
+
+    /* git commands revision */
+  }
+}
